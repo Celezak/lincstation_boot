9cef3451bc217184d6e3d8b7d6d60c63b2a359a9
Karimi <120265555+Celezak@users.noreply.github.com>
2025-01-12 12:25:15 +0100

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Start the LED Service
-chmod 777 /home/fx2mx3/lincstation_boot/led/go_8130_led_linux
-/home/fx2mx3/lincstation_boot/led/go_8130_led_linux >/dev/null 2>&1 &
+chmod 777 /mnt/Fast/Home/lincstation_boot/led/go_8130_led_linux
+/mnt/Fast/Home/lincstation_boot/led/go_8130_led_linux >/dev/null 2>&1 &
 
 # Start the RESET Service
-chmod 777 /home/fx2mx3/lincstation_boot/reset/reset
-/home/fx2mx3/lincstation_boot/reset/reset >/dev/null 2>&1 &
+chmod 777 /mnt/Fast/Home/lincstation_boot/reset/reset
+/mnt/Fast/Home/lincstation_boot/reset/reset >/dev/null 2>&1 &
